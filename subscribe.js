3fdd988c09eb20fe7aca208ed0e373c6a53db8b7
sig <dan@1up.digital>
2020-09-16 09:40:41 +0100

    initial commit

---
+++ subscribe.js
@@ -0,0 +1,40 @@
+var W3CWebSocket = require('websocket').w3cwebsocket;
+ 
+var client = new W3CWebSocket('ws://localhost:8082/pss/subscribe/testtopic');
+ 
+client.onerror = function() {
+    console.log('Connection Error');
+};
+ 
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+ 
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+ 
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+ 
+client.onmessage = function(e) {
+	const textEncoding = require('text-encoding')
+	const td = new textEncoding.TextDecoder("utf-8")
+    const string = td.decode(e.data)
+
+
+	console.log(string)
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+
+    if (typeof e.data === 'ArrayBuffer') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
